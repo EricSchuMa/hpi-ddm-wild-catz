4218fbc0b1726bd0ca99f223a80b0e551c4fc3a4
Max Schumacher <max.schumacher12@googlemail.com>
2019-11-24 12:23:31 +0100

    Added Password Message Structure

--- ddm-pc/src/main/java/de/hpi/ddm/structures/PasswordMessage.java
+++ ddm-pc/src/main/java/de/hpi/ddm/structures/PasswordMessage.java
@@ -1,4 +1,16 @@
 package de.hpi.ddm.structures;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data @NoArgsConstructor @AllArgsConstructor
 public class PasswordMessage {
+    private int id;
+    private String name;
+    private String pchars;
+    private int plength;
+    private String password;
+    private String [] hints;
 }
