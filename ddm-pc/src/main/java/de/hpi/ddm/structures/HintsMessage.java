d9f71711ca3d38c72f6112a51575bc4b5b6a9a6c
Max Schumacher <max.schumacher12@googlemail.com>
2019-11-24 13:52:17 +0100

    Made messages serializable

--- ddm-pc/src/main/java/de/hpi/ddm/structures/HintsMessage.java
+++ ddm-pc/src/main/java/de/hpi/ddm/structures/HintsMessage.java
@@ -1,4 +1,14 @@
 package de.hpi.ddm.structures;
 
-public class HintsMessage {
+import akka.actor.ActorRef;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data @NoArgsConstructor @AllArgsConstructor
+public class HintsMessage implements Serializable {
+    private static final long serialVersionUID = 172844816448627600L;
+    private String[] hints;
 }
--- ddm-pc/src/main/java/de/hpi/ddm/structures/PasswordMessage.java
+++ ddm-pc/src/main/java/de/hpi/ddm/structures/PasswordMessage.java
@@ -1,12 +1,16 @@
 package de.hpi.ddm.structures;
 
+import akka.actor.ActorRef;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 
 @Data @NoArgsConstructor @AllArgsConstructor
-public class PasswordMessage {
+public class PasswordMessage implements Serializable {
+    private static final long serialVersionUID = -50316216448627600L;
     private int id;
     private String name;
     private String pchars;
